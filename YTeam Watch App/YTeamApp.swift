657b74128a82a9c2f78a66a282511f111fe5b409
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-10-10 08:09:30 +0700

    Initial Commit

---
+++ YTeam Watch App/YTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTeamApp.swift
+//  YTeam Watch App
+//
+//  Created by Yap Justin on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YTeam_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YTeam/YTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTeamApp.swift
+//  YTeam
+//
+//  Created by Yap Justin on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
