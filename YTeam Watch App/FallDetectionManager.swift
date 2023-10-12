26799966cdcb3a526c725ae4103f89ebebc4c8bd
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-12 11:02:13 +0700

    Added Documentation for the Fall Detection API

--- YTeam Watch App/FallDetectionManager.swift
+++ YTeam Watch App/FallDetectionManager.swift
@@ -16,14 +16,33 @@ class FallDetectionManager: NSObject, CMFallDetectionDelegate, ObservableObject 
     override init() {
         super.init()
         self.assignDelegate()
-        self.checkForAuthorizationStatus()
+        self.checkAndRequestForAuthorizationStatus()
     }
     
+    /// Assign fall detection delegate to `watch for fall detection events`.
+    ///
+    /// ```
+    /// FallDetectionManager.assignDelegate()
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - None
+    /// - Returns: None, the function sets the fall detector's delegate to self.
     func assignDelegate() {
         self.fallDetector.delegate = self
     }
     
-    func checkForAuthorizationStatus() {
+    /// Check the watch's `fall data authorization status` and `asks for confirmation in authorization`.
+    ///
+    /// ```
+    /// FallDetectionManager.checkAndRequestForAuthorizationStatus()
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - None
+    /// - Returns: None, the function asks for authorization status and sets the authorization status to user's input and
+    ///  `sets the authorization boolean` in the ObservedObject.
+    func checkAndRequestForAuthorizationStatus() {
         if self.fallDetector.authorizationStatus == .authorized {
             self.authorized = true
         } else {
@@ -39,12 +58,30 @@ class FallDetectionManager: NSObject, CMFallDetectionDelegate, ObservableObject 
         }
     }
     
+    /// `Unchangable conforming function to automatically check for falls`.
+    ///
+    /// ```
+    /// Not Called. Leave it be.
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - None
+    /// - Returns: None.
     func fallDetectionManager(
         _ fallDetectionManager: CMFallDetectionManager,
         didDetect event: CMFallDetectionEvent) async {
         print("Fall Detected!", event.date, event.resolution.rawValue)
     }
     
+    /// `Unchangable conforming function to automatically check for change in authorization`.
+    ///
+    /// ```
+    /// Not Called. Leave it be.
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - None
+    /// - Returns: None.
     func fallDetectionManagerDidChangeAuthorization(
         _ fallDetectionManager: CMFallDetectionManager
     )  {
