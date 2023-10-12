95e95aa37b7e04ea31b7356f6a5dec0f0275cbfe
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-12 09:01:05 +0700

    Added Settings for Fall Detection Notifications

---
+++ YTeam Watch App/FallDetectionManager.swift
@@ -0,0 +1,20 @@
+//
+//  FallDetectionManager.swift
+//  YTeam Watch App
+//
+//  Created by Kenny Jinhiro Wibowo on 12/10/23.
+//
+
+import Foundation
+import CoreMotion
+
+class FallDetectionManager: ObservableObject {
+    private var fallDetector: CMFallDetectionManager = CMFallDetectionManager()
+    
+//    init() {
+//        if CMFallDetectionManager.isAvailable  {
+//            var fallDetector = CMFallDetectionManager()
+//        }
+//    }
+    
+}
