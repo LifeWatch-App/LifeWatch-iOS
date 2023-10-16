675d9c5da8e7a951d0266c9b79b595998a79f932
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-16 08:22:57 +0700

    Reverted ContentView

--- YTeam Watch App/ContentView.swift
+++ YTeam Watch App/ContentView.swift
@@ -8,13 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var fallDetectionManager: FallDetectionManager = FallDetectionManager()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            fallDetectionManager.authorized ? Text("Fall Detection Authorized, Try Falling!") : Text("Authorize First")
+            Text("Hello, World!")
         }
         .padding()
     }
