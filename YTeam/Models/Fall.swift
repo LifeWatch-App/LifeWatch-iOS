7eb12962b83b57d4bcff6e388190d58d5b7203a1
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-17 17:00:21 +0700

    Added FallModel, Service, and Fetch

---
+++ YTeam/Models/Fall.swift
@@ -0,0 +1,13 @@
+//
+//  Fall.swift
+//  YTeam
+//
+//  Created by Kenny Jinhiro Wibowo on 17/10/23.
+//
+
+import Foundation
+
+struct Fall: Codable, Hashable {
+    let seniorId: String
+    let time: String
+}
