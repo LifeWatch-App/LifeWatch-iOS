578d0f4d6be3c789b0c74b9cdc69645b071eeb59
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-24 12:57:41 +0700

    Update HistoryViewModel.swift

--- YTeam/ViewModels/HistoryViewModel.swift
+++ YTeam/ViewModels/HistoryViewModel.swift
@@ -2,7 +2,7 @@
 //  HistoryViewModel.swift
 //  YTeam
 //
-//  Created by Maximus Aurelius Wiranata on 16/10/23.
+//  Created by Maximus Aurelius Wiranata and Kenny Jinhiro on 16/10/23.
 //
 
 import SwiftUI
