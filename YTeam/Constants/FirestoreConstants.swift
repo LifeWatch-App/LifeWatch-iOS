bbbbcabe6d57d10100cfcb48d9dbf26bf523c297
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-10-25 19:18:43 +0700

    Added missing Firestore constants

--- YTeam/Constants/FirestoreConstants.swift
+++ YTeam/Constants/FirestoreConstants.swift
@@ -17,5 +17,7 @@ struct FirestoreConstants {
     static let chargesCollection = Firestore.firestore().collection("charges")
     static let sosCollection = Firestore.firestore().collection("sos")
     static let idlesCollection = Firestore.firestore().collection("idles")
+    static let batteryLevelCollection = Firestore.firestore().collection("batteryLevels")
+    static let chargingCollection = Firestore.firestore().collection("charging")
 }
 
