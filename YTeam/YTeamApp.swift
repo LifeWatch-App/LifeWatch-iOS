ba73e81b8e2d6f70830636c18363da40fdfa1520
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-10-31 11:27:02 +0700

    Added notifications in foreground

--- YTeam/YTeamApp.swift
+++ YTeam/YTeamApp.swift
@@ -24,6 +24,14 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         application.registerForRemoteNotifications()
         return true
     }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        completionHandler(.banner)
+    }
 }
 
 extension AppDelegate: MessagingDelegate {
