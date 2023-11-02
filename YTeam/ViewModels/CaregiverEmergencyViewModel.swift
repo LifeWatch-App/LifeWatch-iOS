fa9d837d6ea453fef8bc97766471a91e336df4e1
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-11-02 14:13:03 +0700

    Added voice recording

--- YTeam/ViewModels/CaregiverEmergencyViewModel.swift
+++ YTeam/ViewModels/CaregiverEmergencyViewModel.swift
@@ -8,16 +8,22 @@
 import Foundation
 import Combine
 import FirebaseAuth
+import AVFoundation
 
-class CaregiverEmergencyViewModel: ObservableObject {
+class CaregiverEmergencyViewModel: NSObject, ObservableObject, AVAudioPlayerDelegate  {
     @Published var invites: [Invite] = []
     @Published var user: User?
     @Published var userData: UserData?
     private let service = AuthService.shared
     private var cancellables = Set<AnyCancellable>()
+    var recorder : AVAudioRecorder!
+    var audioPlayer : AVAudioPlayer!
+    @Published var recordingsList = [URL]()
 
-    init() {
+    override init() {
+        super.init()
         setupSubscribers()
+        fetchAllRecording()
     }
 
     private func setupSubscribers() {
@@ -37,5 +43,113 @@ class CaregiverEmergencyViewModel: ObservableObject {
     
     func signOut() {
         AuthService.shared.signOut()
+    }
+    
+    func startRecording(){
+            
+            let recordingSession = AVAudioSession.sharedInstance()
+            do {
+                try recordingSession.setCategory(.playAndRecord, mode: .default)
+                try recordingSession.setActive(true)
+            } catch {
+                print("Can not setup the Recording")
+            }
+            
+            let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let fileName = path.appendingPathComponent("CO-Voice : test.caf")
+            
+            
+            
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            
+            do {
+                recorder = try AVAudioRecorder(url: fileName, settings: settings)
+                recorder.prepareToRecord()
+                recorder.record()
+                
+            } catch {
+                print("Failed to Setup the Recording")
+            }
+        }
+    
+    func stopRecording() {
+        recorder.stop()
+    }
+    
+    func fetchAllRecording(){
+            
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryContents = try! FileManager.default.contentsOfDirectory(at: path, includingPropertiesForKeys: nil)
+
+        for i in directoryContents {
+            recordingsList.append(i)
+        }
+            
+    }
+    
+    func startPlaying(url : URL) {
+      
+        let playSession = AVAudioSession.sharedInstance()
+            
+        do {
+            try playSession.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        } catch {
+            print("Playing failed in Device")
+        }
+            
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf : url)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+                
+            for i in 0..<recordingsList.count{
+                if recordingsList[i] == url{
+                    /*ecordingsList[i].isPlaying = true*/
+                }
+            }
+                
+        } catch {
+            print("Playing Failed")
+            print(error)
+        }
+                
+    }
+
+    func stopPlaying(url : URL){
+      
+        audioPlayer.stop()
+      
+        for i in 0..<recordingsList.count {
+            if recordingsList[i] == url {
+//                recordingsList[i].isPlaying = false
+            }
+        }
+      
+    }
+    
+    func deleteRecording(url : URL){
+        
+        do {
+            try FileManager.default.removeItem(at : url)
+        } catch {
+            print("Can't delete")
+        }
+            
+        for i in 0..<recordingsList.count {
+            
+            if recordingsList[i] == url {
+                stopPlaying(url: recordingsList[i])
+                
+                recordingsList.remove(at : i)
+                    
+                break
+            }
+        }
     }
 }
